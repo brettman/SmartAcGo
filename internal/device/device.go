65eeb06e13bac07d82ec576423cd489e547d61aa
brettman <brettman@gmail.com>
2021-11-21 16:31:40 +0200

    this one is controversial for me... I don't want the ORM or the DB to bleed into the business domain model.  We should probably have separate types for the db model and the domain model, but in the interest of getting a prototype built, I'm going to leave just a unified model for now.

--- internal/device/device.go
+++ internal/device/device.go
@@ -1,20 +1,25 @@
 package device
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 // Device - an air conditioner which will submit data readings
 type Device struct {
-	SerialNr         string
+	gorm.Model
+	SerialNr         string `gorm:primaryKey`
 	FirmwareVersion  string
 	RegistrationDate time.Time
 	SensorLogs       []SensorLogEntry
 }
 
 type SensorLogEntry struct {
+	gorm.Model
 	SerialNr     string
 	Temperature  float32
 	Humidity     float32
 	COLevel      float32
-	DeviceHealth string
+	DeviceHealth string `gorm:size:50`
 	Timestamp    time.Time
 }
